00a7d62f32bf833dc92f96775c948253941b271e
Vadim Volkov <antibiotik15@ya.ru>
2021-03-23 10:57:27 +0300

    create new project

---
+++ src/main/java/by/volkov/restaurantvoiting/RestaurantVoitingApplication.java
@@ -0,0 +1,13 @@
+package by.volkov.restaurantvoiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVoitingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVoitingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/volkov/restaurantvoiting/RestaurantVoitingApplicationTests.java
@@ -0,0 +1,13 @@
+package by.volkov.restaurantvoiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVoitingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
