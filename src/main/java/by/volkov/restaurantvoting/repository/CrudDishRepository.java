741c3eb294736c8b72576d11bf8f717282a859b1
Vadim Volkov <antibiotik15@ya.ru>
2021-03-25 22:03:15 +0300

    add CrudDishRepository

---
+++ src/main/java/by/volkov/restaurantvoting/repository/CrudDishRepository.java
@@ -0,0 +1,20 @@
+package by.volkov.restaurantvoting.repository;
+
+import by.volkov.restaurantvoting.model.Dish;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional(readOnly = true)
+public interface CrudDishRepository extends JpaRepository<Dish, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Dish d WHERE d.restaurant.user.id=:userId AND d.restaurant.id=:restaurantId AND d.id=:id")
+    int delete(int id, int restaurantId, int userId);
+
+    List<Dish> getAllByRestaurantId(int restaurantId);
+}
