62ebdc023f58776ca808ef78929d9d9b37d8f67f
Vadim Volkov <antibiotik15@ya.ru>
2021-03-25 20:57:29 +0300

    add CrudUserRepository

---
+++ src/main/java/by/volkov/restaurantvoting/repository/CrudUserRepository.java
@@ -0,0 +1,26 @@
+package by.volkov.restaurantvoting.repository;
+
+import by.volkov.restaurantvoting.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional(readOnly = true)
+public interface CrudUserRepository extends JpaRepository<User, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM User u WHERE u.id=:id")
+    int delete(int id);
+
+    User getByEmail(String email);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE User u SET u.voted=true, u.votedRestarauntId=:votedRestaurantId WHERE u.id=:id")
+    User vote(int id, int votedRestaurantId);
+
+    @Query("SELECT u.voted FROM User u WHERE u.id=:id")
+    boolean hasVoted(int id, boolean voted);
+}
