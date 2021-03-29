e2d7d317d1420addc8112c49fd55df6690664af0
Vadim Volkov <antibiotik15@ya.ru>
2021-03-29 10:33:09 +0300

    add ValidationUtil

---
+++ src/main/java/by/volkov/restaurantvoting/util/ValidationUtil.java
@@ -0,0 +1,43 @@
+package by.volkov.restaurantvoting.util;
+
+import by.volkov.restaurantvoting.model.BaseEntity;
+import by.volkov.restaurantvoting.util.exception.NotFoundException;
+
+public class ValidationUtil {
+    private ValidationUtil() {
+    }
+
+    public static <T> T checkNotFoundWithId(T object, int id) {
+        checkNotFoundWithId(object != null, id);
+        return object;
+    }
+
+    public static void checkNotFoundWithId(boolean found, int id) {
+        checkNotFound(found, "id=" + id);
+    }
+
+    public static <T> T checkNotFound(T object, String msg) {
+        checkNotFound(object != null, msg);
+        return object;
+    }
+
+    public static void checkNotFound(boolean found, String msg) {
+        if (!found) {
+            throw new NotFoundException("Not found entity with " + msg);
+        }
+    }
+
+    public static void checkNew(BaseEntity entity) {
+        if (!entity.isNew()) {
+            throw new IllegalArgumentException(entity + " must be new (id=null)");
+        }
+    }
+
+    public static void assureIdConsistent(BaseEntity entity, int id) {
+        if (entity.isNew()) {
+            entity.setId(id);
+        } else if (entity.getId() != id) {
+            throw new IllegalArgumentException(entity + " must be with id=" + id);
+        }
+    }
+}
---
+++ src/main/java/by/volkov/restaurantvoting/util/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package by.volkov.restaurantvoting.util.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
