9366fb36a7fc5d591bef5a17afa447ad05c4016a
Vadim Volkov <antibiotik15@ya.ru>
2021-03-31 13:08:25 +0300

    fix error in pom

--- src/main/java/by/volkov/restaurantvoting/RestaurantVotingApplication.java
+++ src/main/java/by/volkov/restaurantvoting/RestaurantVotingApplication.java
@@ -1,13 +1,25 @@
 package by.volkov.restaurantvoting;
 
+import by.volkov.restaurantvoting.repository.CrudUserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestaurantVotingApplication {
+@AllArgsConstructor
+public class RestaurantVotingApplication implements ApplicationRunner {
+    private final CrudUserRepository repository;
 
     public static void main(String[] args) {
         SpringApplication.run(RestaurantVotingApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(repository.hasVoted(1));
+        System.out.println(repository.getByEmail("user@gmail.com"));
+        System.out.println(repository.findAll());
+    }
 }
